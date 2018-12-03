1f158baa295758f7513d845f99c864e1a1b771f1
Neiwert, Dean D <dean.neiwert@penfed.org>
2018-12-02 16:47:01 -0800

    Module 3 completed

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,22 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li @for="book in books">{{ book.title }}: {{ book.author }}</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   name: "BookList",
   data(){
     return {
-      title: "All Books"
+      title: "All Books",
+      books: [
+        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+        {title: 'American Gods', author: 'Neil Gaiman'},
+        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ],
     };
   },
 }
@@ -14,5 +24,13 @@ export default {
 <style>
   h1, h2{
     font-weight: normal;
+  }
+  ol{
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    display: block;
+    margin: 0 10px;
   }
 </style>
